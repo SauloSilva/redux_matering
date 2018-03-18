0e72d75670a2f926be60de6f4c659bcb1b1fa25c
Saulo Santiago <saulodasilvasantiago@gmail.com>
2018-03-18 16:15:43 -0300

    configure new app

--- src/index.js
+++ src/index.js
@@ -6,17 +6,16 @@
 
 import React from 'react';
 import { Provider } from 'react-redux';
+import { View } from 'react-native';
 
 import 'config/ReactotronConfig';
 import 'config/DevToolsConfig';
 
 import store from 'store';
 
-import TodoList from 'TodoList';
-
 const App = () => (
   <Provider store={store}>
-    <TodoList />
+    <View />
   </Provider>
 );
 
