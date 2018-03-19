35592d03d3d93196a6ee1386e06f285040f9c3ea
Saulo Santiago <saulodasilvasantiago@gmail.com>
2018-03-19 02:28:26 -0300

    add ducks concepts

--- src/store/sagas/favorites.js
+++ src/store/sagas/favorites.js
@@ -1,13 +1,19 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import api from 'services/api';
 
-import { addFavoriteSuccess, addFavoriteError } from 'store/actions/favorites';
+import { Creators as FavoriteActions } from 'store/ducks/favorites';
 
 export function* addFavoriteRequest(action) {
   try {
     const response = yield call(api.get, `/repos/${action.payload.repoName}`);
-    yield put(addFavoriteSuccess(response.data));
+    const favorites = yield select(state => state.favorites.data);
+
+    if (favorites.find(favorite => favorite.id === response.data.id)) {
+      yield put(FavoriteActions.addFavoriteError('Repositório já foi adicionado!!!'));
+    } else {
+      yield put(FavoriteActions.addFavoriteSuccess(response.data));
+    }
   } catch (err) {
-    yield put(addFavoriteError('Repositório não existe!!!'));
+    yield put(FavoriteActions.addFavoriteError('Repositório não existe!!!'));
   }
 }
