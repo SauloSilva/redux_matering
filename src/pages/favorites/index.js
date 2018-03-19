cc9afba2c07a38e8d7c98de68dced2be6a89f6ab
Saulo Santiago <saulodasilvasantiago@gmail.com>
2018-03-19 00:54:57 -0300

    add style and components of favorites view

--- src/pages/favorites/index.js
+++ src/pages/favorites/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, FlatList } from 'react-native';
 
-const Favorites = () => <View />;
+import FavoriteItem from './components/FavoriteItem';
 
-Favorites.navigationOptions = {
-  title: 'Meus favoritos',
-};
+import styles from './styles';
 
-export default Favorites;
+export default class Favorites extends Component {
+  static navigationOptions = {
+    title: 'Meus favoritos',
+  };
+
+  state = {
+    favorites: [{
+      owner: {
+        avatar_url: 'https://avatars1.githubusercontent.com/u/24914645?s=200&v=4',
+      },
+      name: 'Foo',
+      description: 'bar lorem ipsum',
+    }],
+  };
+
+  renderList = () => (
+    <FlatList
+      data={this.state.favorites}
+      keyExtractor={item => String(item.id)}
+      renderItem={({ item }) => <FavoriteItem favorite={item} />}
+    />
+  )
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {
+          !this.state.favorites.length
+            ? <Text style={styles.empty}>Nenhum favorito adicionado</Text>
+            : this.renderList()
+        }
+      </View>
+    );
+  }
+}
